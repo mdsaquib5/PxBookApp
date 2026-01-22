073b8e5c7794cb2b9791ac703e5f3753f2d335e6
Md Saquib <mdsaquib5540@gmail.com>
2026-01-22 22:49:53 +0530

    okay

--- src/components/ui/BestSeller.jsx
+++ src/components/ui/BestSeller.jsx
@@ -18,6 +18,37 @@ const BestSeller = () => {
         loop: false,
         speed: 600,
         grabCursor: true,
+        breakpoints: {
+            // 1800px → 1440px
+            1440: {
+                slidesPerView: 2,
+            },
+            1240: {
+                slidesPerView: 2,
+            },
+            // 1240px → 1024px
+            1024: {
+                slidesPerView: 1,
+            },
+            // 1024px → 992px
+            992: {
+                slidesPerView: 1,
+            },
+
+            // 992px → 768px
+            768: {
+                slidesPerView: 2,
+            },
+            // 768px → 576px
+            576: {
+                slidesPerView: 1,
+            },
+
+            // below 576px
+            0: {
+                slidesPerView: 1,
+            }
+        }
     };
 
     return (
@@ -28,10 +59,10 @@ const BestSeller = () => {
                         <div className="sectionHeading">Best Seller</div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat tempora ipsa iusto cum provident saepe vero enim accusantium, labore, error consequuntur ea laboriosam a odio itaque aliquid animi porro ad.</p>
                     </div>
-                    <div className="sellerGrid">
+                    <div className="sellerTestimonial">
                         <Swiper {...bestSellerConfig}>
-                            {sellerData.map((item, index) => (
-                                <SwiperSlide key={index}>
+                            {sellerData.map((item) => (
+                                <SwiperSlide key={item.id}>
                                     <SellerCard item={item} />
                                 </SwiperSlide>
                             ))}
