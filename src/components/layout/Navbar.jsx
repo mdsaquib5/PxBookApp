7a467bfc9dd71907c3d45b37694e7c41c252154f
Md Saquib <mdsaquib5540@gmail.com>
2026-01-22 19:02:52 +0530

    header-fixed

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,9 +3,16 @@ import { useState, useEffect } from "react";
 import { CiSearch } from "react-icons/ci";
 import { FaChevronDown } from "react-icons/fa6";
 import { PiShoppingCart } from "react-icons/pi";
+import { useMediaQuery } from "react-responsive";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { CgClose } from "react-icons/cg";
 
 const Navbar = () => {
+
+    const isTablet = useMediaQuery({ query: "(min-width: 1240px)" });
+
     const [showMenu, setShowMenu] = useState(false);
+    const [showPanel, setShowPanel] = useState(false);
     const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
@@ -24,33 +31,95 @@ const Navbar = () => {
                                 <img src="/assets/images/header-logo.webp" alt="Header Logo" />
                             </Link>
                         </div>
-                        <div className="searchMenu">
-                            <div className="menu">
-                                <div className="menu-btn" onClick={() => setShowMenu(!showMenu)}>
-                                    <span>Menu</span> <FaChevronDown />
-                                    {showMenu && (
-                                        <ul className="menuItems">
-                                            <li><Link to={'/'}>Home</Link></li>
-                                            <li><Link to={'/'}>About</Link></li>
-                                            <li><Link to={'/'}>Contact</Link></li>
-                                            <li><Link to={'/'}>Login</Link></li>
-                                            <li><Link to={'/'}>Register</Link></li>
-                                        </ul>
-                                    )}
+                        {isTablet && (
+                            <div className="searchMenu">
+                                <div className="menu">
+                                    <div
+                                        className="menu-btn"
+                                        onClick={() => setShowMenu(!showMenu)}
+                                    >
+                                        <span>Menu</span> <FaChevronDown />
+
+                                        {showMenu && (
+                                            <ul className="menuItems">
+                                                <li><Link to="/">Home</Link></li>
+                                                <li><Link to="/">About</Link></li>
+                                                <li><Link to="/">Contact</Link></li>
+                                                <li><Link to="/">Login</Link></li>
+                                                <li><Link to="/">Register</Link></li>
+                                            </ul>
+                                        )}
+                                    </div>
+                                </div>
+
+                                <div className="search">
+                                    <form>
+                                        <input type="text" placeholder="Find books here..." />
+                                        <button type="submit">
+                                            <CiSearch />
+                                        </button>
+                                    </form>
                                 </div>
                             </div>
-                            <div className="search">
-                                <form action="">
-                                    <input type="text" placeholder="Find books here..." />
-                                    <button><CiSearch /></button>
-                                </form>
-                            </div>
-                        </div>
+                        )}
                         <div className="headerBtns">
-                            <div className="header-right">
-                                <div className="cartIcon"><PiShoppingCart /></div>
-                                <Link className="btn signInBtn" to={'/'}>Sign In</Link>
-                                <button className="btn ctaBtn">Create Account</button>
+                            <div className="headerRight">
+                                <Link className="cartIcon" to={'/'}><PiShoppingCart /></Link>
+                                {isTablet && (
+                                    <>
+                                        <Link className="btn signInBtn" to={'/'}>Sign In</Link>
+                                        <button className="btn ctaBtn">Create Account</button>
+                                    </>
+                                )}
+                                {
+                                    !isTablet && (
+                                        <div className="mobileDrawer">
+                                            {/* Drawer Toggle Button */}
+                                            <button
+                                                className="drawerToggle"
+                                                onClick={() => setShowPanel(!showPanel)}
+                                            >
+                                                <RxHamburgerMenu />
+                                            </button>
+
+                                            {/* Drawer Panel */}
+                                            <div className={`drawerPanel ${showPanel ? "open" : ""}`}>
+                                                <button
+                                                    className="drawerClose"
+                                                    onClick={() => setShowPanel(false)}
+                                                >
+                                                    <CgClose />
+                                                </button>
+                                                <div className="panel">
+                                                    <div className="search">
+                                                        <form action="">
+                                                            <input type="text" placeholder="Find books here..." />
+                                                            <button><CiSearch /></button>
+                                                        </form>
+                                                    </div>
+                                                    <div className="menu">
+                                                        <div className="menu-btn" onClick={() => setShowMenu(!showMenu)}>
+                                                            <span>Menu</span> <FaChevronDown />
+                                                        </div>
+                                                        {showMenu && (
+                                                            <ul className="menuItems">
+                                                                <li><Link to={'/'}>Home</Link></li>
+                                                                <li><Link to={'/'}>About</Link></li>
+                                                                <li><Link to={'/'}>Contact</Link></li>
+                                                                <li><Link to={'/'}>Login</Link></li>
+                                                                <li><Link to={'/'}>Register</Link></li>
+                                                            </ul>
+                                                        )}
+                                                    </div>
+                                                    <div className="navItems">
+                                                        <Link className="btn signInBtn" to={'/'}>Sign In</Link>
+                                                        <button className="btn ctaBtn">Create Account</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
                             </div>
                         </div>
                     </div>
